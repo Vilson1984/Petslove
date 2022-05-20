06a86170b82de8d5916e9375c85b331c06e9c2ad
Vilson1984 <vcmorion@hotmail.com>
2022-05-20 09:19:51 -0300

    Validation Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+ 
 class User < ApplicationRecord
+    #HAS_ CRIARÁ HASHS PARA A SENHA, PROTEÇÃO EM CASO DE ATAQUE
+    has_secure_password 
+
+    #VAI USAR UMA EXPRESSÃO REGULAR PARA VALIDAR O EMAIL
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    validates :name, presence:true, length:{maximum:50} #NÃO SE PODE CRIAR OUTRO USUÁRIO SEM ANTES PASSAR O NOME
+    validates :password, length:{minimum:6}
+    validates :email, presence:true, length:{maximum:255},
+                                     format:{with:VALID_EMAIL_REGEX}, 
+                                     uniqueness:{case_sensitive:true}
+
+
 end
