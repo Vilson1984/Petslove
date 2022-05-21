07cc788b82629ac2e292106996c89382497514ee
Vilson1984 <vcmorion@hotmail.com>
2022-05-21 10:41:36 -0300

    Message Cadastro Sucesso

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params) #CRIAMOS A VARIÁVEL USER E INSTANCIAMOS ESSE OBJETO COM PARAMS, ESSES PARÂMETROS ESTÃO DECLARADOS NO PRIVATE
+
+       if @user.save
+        flash[:sucess] = 'Usuário cadastrado com sucesso!' #FLASH É UM HASH QUE O RAILS DISPONIBILIZA PARA MANDAR MSGS PARA O USUÁRIO
+        redirect_to root_url #REDICIONARÁ O USUÁRIO PARA A PÁGINA INDEX
+       else
+           render 'new' #SENÃO REDIRECIONA PARA A PÁGINA NEW 
+       end
+    end
+
+    private 
+    # A FUNÇÃO VAI FILTRAR OS PARÂMETROS EVITAR QUE O USUÁRIO INJETE UM PARAMETRO DESCONHECIDO CAUSANDO
+    # COMPORTAMENTO INDESEJÁVEL.
+    # CHAMAREMOS A VARIAVEL PARAMS. CHAMAREMOS O METODO REQUIRE PARA OBRIGAR O USUÁRIO A PASSAR 
+    # DENTRO DA CHAVE(:USER) OS PARAMETROS EMAIL, NAME, PASSWORD E PASSAWORD_DIGEST         
+    def user_params
+        params.require(:user).permit(:email, :name, :password, :password_confirmation)
     end
 end
